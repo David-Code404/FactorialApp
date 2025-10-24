b4351fafde21a03abc36666f08fc015d7bb758f6
David-Code404 <josesucullani@gmail.com>
2025-10-24 07:09:16 -1200

    add actualizacion

--- Factorial/src/main/java/com/mycompany/factorial/Factorial.java
+++ Factorial/src/main/java/com/mycompany/factorial/Factorial.java
@@ -7,3 +7,20 @@ package com.mycompany.factorial;
  *
  * @author joses
  */
+
+
+
+public class Factorial {
+
+    public static long calcular(int numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("El número debe ser >= 0");
+        }
+        long resultado = 1;
+
+        for (int i = 2; i <= numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
